e15a39e65c9419d431216ed0f19ee9343bb0c037
Mike Ruangutai <mruangutai@ign.com>
2012-03-30 21:51:59 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectGoldeneye::Application.config.secret_token = '40f1662865ab125ef0d39b23df9325ab23744ddf42c049e3be1ddfbc336e58b4cebc0b92b37a1aff6c9d33e1df61e069186ed685ca5585cd749b25a865a4d34a'

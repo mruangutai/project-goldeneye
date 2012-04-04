0e25308e8ebd1191cc44e288192afc03d85734e0
joesomozawork <joesomoza.work@gmail.com>
2012-04-04 15:26:22 -0700

    Add the views for _form, edit, and new
    We are adding our own fake data using rails.

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -21,63 +21,63 @@ class CodesController < ApplicationController
     end
   end
 
-  # # GET /codes/new
-  # # GET /codes/new.json
-  # def new
-  #   @code = Code.new
-  # 
-  #   respond_to do |format|
-  #     format.html # new.html.erb
-  #     format.json { render json: @code }
-  #   end
-  # end
-  # 
-  # # GET /codes/1/edit
-  # def edit
-  #   @code = Code.find(params[:id])
-  # end
-  # 
-  # # POST /codes
-  # # POST /codes.json
-  # def create
-  #   @code = Code.new(params[:code])
-  # 
-  #   respond_to do |format|
-  #     if @code.save
-  #       format.html { redirect_to @code, notice: 'Code was successfully created.' }
-  #       format.json { render json: @code, status: :created, location: @code }
-  #     else
-  #       format.html { render action: "new" }
-  #       format.json { render json: @code.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-  # 
-  # # PUT /codes/1
-  # # PUT /codes/1.json
-  # def update
-  #   @code = Code.find(params[:id])
-  # 
-  #   respond_to do |format|
-  #     if @code.update_attributes(params[:code])
-  #       format.html { redirect_to @code, notice: 'Code was successfully updated.' }
-  #       format.json { head :ok }
-  #     else
-  #       format.html { render action: "edit" }
-  #       format.json { render json: @code.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-  # 
-  # # DELETE /codes/1
-  # # DELETE /codes/1.json
-  # def destroy
-  #   @code = Code.find(params[:id])
-  #   @code.destroy
-  # 
-  #   respond_to do |format|
-  #     format.html { redirect_to codes_url }
-  #     format.json { head :ok }
-  #   end
-  # end
+  # GET /codes/new
+  # GET /codes/new.json
+  def new
+    @code = Code.new
+  
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @code }
+    end
+  end
+  
+  # GET /codes/1/edit
+  def edit
+    @code = Code.find(params[:id])
+  end
+  
+  # POST /codes
+  # POST /codes.json
+  def create
+    @code = Code.new(params[:code])
+  
+    respond_to do |format|
+      if @code.save
+        format.html { redirect_to @code, notice: 'Code was successfully created.' }
+        format.json { render json: @code, status: :created, location: @code }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @code.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  # PUT /codes/1
+  # PUT /codes/1.json
+  def update
+    @code = Code.find(params[:id])
+  
+    respond_to do |format|
+      if @code.update_attributes(params[:code])
+        format.html { redirect_to @code, notice: 'Code was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @code.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  # DELETE /codes/1
+  # DELETE /codes/1.json
+  def destroy
+    @code = Code.find(params[:id])
+    @code.destroy
+  
+    respond_to do |format|
+      format.html { redirect_to codes_url }
+      format.json { head :ok }
+    end
+  end
 end

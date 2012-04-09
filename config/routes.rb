5b8cdc3843546db4cfab3e19149484aad381f73b
Lee Munroe <lee@leemunroe.com>
2012-04-09 11:18:47 -0700

    Changed back link on show to root. Changed roots so pagination URL is on root and not /codes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ProjectGoldeneye::Application.routes.draw do
+
+  root :to => 'codes#index'
+  
   resources :codes
 
   # The priority is based upon order of creation:
@@ -50,7 +53,7 @@ ProjectGoldeneye::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'codes#index'
+  
 
   # See how all your routes lay out with "rake routes"
 

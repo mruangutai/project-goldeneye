2915ef6a1c793e966afbb9286e3942466391f63f
joesomozawork <joesomoza.work@gmail.com>
2012-05-02 23:03:08 -0700

    Return records that contain multiple words

--- app/models/code.rb
+++ app/models/code.rb
@@ -3,18 +3,31 @@ class Code < ActiveRecord::Base
   friendly_id :short, use: [:slugged, :history]
 
   
-  def self.search(search, page)
+  def self.search( search, page )
     max_per_page = 20
     if search
-      paginate :per_page => max_per_page, :page => page,
-               :conditions => [
-                  'code ILIKE :search OR long ILIKE :search OR short ILIKE :search',
-                  {:search => "%#{search}%"}
-                ],
-               :order => 'code'
+      where( to_search( 'code',  search ) + ' OR ' + to_search( 'long',  search ) + ' OR ' + to_search( 'short',  search ) )
+      .paginate( :per_page => max_per_page, :page => page, :order => 'code' )      
     else
-      paginate :per_page => max_per_page, :page => page, :order => 'code'
+      paginate( :per_page => max_per_page, :page => page, :order => 'code' )
     end
+  end
+
+
+  def self.to_search( field, search )
+    tokens = search.split( /[,\s]+/ )
+    n = tokens.length
+    i = 0
+    result = ''
+    while i < n
+      if i == 0
+        result = field + " ILIKE '%" + tokens[i] + "%'"
+      else
+        result = result + " OR " + field + " ILIKE '%" + tokens[i] + "%'"
+      end
+      i = i + 1
+    end 
+    result
   end
 
 

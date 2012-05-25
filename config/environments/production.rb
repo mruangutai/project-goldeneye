7783d53cb74f5316b27e452409978b9bb8cabb2a
joesomozawork <joesomoza.work@gmail.com>
2012-05-24 17:20:57 -0700

    Use gzip for speed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 ProjectGoldeneye::Application.configure do
+  config.middleware.use Rack::Deflater
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests

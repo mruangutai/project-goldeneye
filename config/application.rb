8b4f72f32e7b0bd05442f2409b049d68645ef0c7
joesomozawork <joesomoza.work@gmail.com>
2012-05-24 18:00:28 -0700

    Use gzip for speed

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ end
 
 module ProjectGoldeneye
   class Application < Rails::Application
-    config.middleware.use Rack::Deflater
-    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
